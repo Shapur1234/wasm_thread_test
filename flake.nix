ae88ff1c961f3d7e68b7c9b7ff96b5f0f5ae06d6
Shapur1234 <48966182+Shapur1234@users.noreply.github.com>
2024-01-30 16:26:33 +0100

    +

--- flake.nix
+++ flake.nix
@@ -158,8 +158,9 @@
           # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages = [
-            pkgs.trunk
+          packages = with pkgs; [
+            wasm-pack
+            trunk
           ];
         };
       });
